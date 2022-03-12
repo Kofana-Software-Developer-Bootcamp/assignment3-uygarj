e6303ad6ac4833b1dfd4ef8859ca526f8f012c77
Uygar <98343558+uygarj@users.noreply.github.com>
2022-03-12 13:59:52 +0300

    Add files via upload

---
+++ sipariş3/sipariş3/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace sipariş3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+
+		}
+
+		private void Form1_Load_1(object sender, EventArgs e)
+		{
+			//SoundPlayer fifa = new SoundPlayer();
+			//string oyun = Application.StartupPath + "\\ses.wav";
+			//fifa.SoundLocation = oyun;
+			//fifa.Play();
+			//this.FormBorderStyle = FormBorderStyle.FixedSingle;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (textBox3.Text == "1234")
+			{
+				MessageBox.Show("Serkan İnan your order is already completed", "İNFO");
+			}
+			else if (textBox3.Text == "5678")
+			{
+				MessageBox.Show("Uygar İde your order is already completed", "İNFO");
+			}
+			else if (textBox3.Text == "9012")
+			{
+				MessageBox.Show("Aykut Baş your order is already completed", "İNFO");
+			}
+			else if (textBox3.Text == "")
+			{
+				MessageBox.Show("Cannot be Blank!");
+			}
+			else
+			{
+				Frm2 frm2 = new Frm2();
+				frm2.Show();
+				this.Hide();
+			}
+		}
+
+		private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if(!char.IsDigit(e.KeyChar) && e.KeyChar !=8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
---
+++ sipariş3/sipariş3/Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sipariş3
+{
+	public partial class Frm2 : Form
+	{
+		public Frm2()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string tc = "", AdSoyad = "", telefon = "", urun = "", tane = "";
+			tc = textBox1.Text;
+			AdSoyad = textBox2.Text;
+			telefon = textBox3.Text;
+			tane = textBox4.Text;
+			urun = comboBox1.Text;
+
+			MessageBox.Show("Registration Completed", "İNFO");
+
+			listBox1.Items.Add(tc);
+			listBox2.Items.Add(AdSoyad);
+			listBox3.Items.Add(telefon);
+			listBox4.Items.Add(urun);
+			listBox5.Items.Add(tane);
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (char.IsDigit(e.KeyChar))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void Frm2_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
